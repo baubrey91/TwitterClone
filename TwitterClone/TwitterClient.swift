79fcab7b62a56f37b2b256e8855a989e217b736f
Brandon <brandon@brandons-macbook-pro.local>
2017-04-11 12:03:44 -0700

    video 4 done

--- TwitterClone/TwitterClient.swift
+++ TwitterClone/TwitterClient.swift
@@ -17,6 +17,7 @@ let authorize = "/oauth/authorize?oauth_token="
 
 class TwitterClient: BDBOAuth1SessionManager {
     
+    
     static let sharedInstance = TwitterClient(baseURL: URL(string: "https://api.twitter.com")! as URL!,
                                                         consumerKey: consumerKey,
                                                         consumerSecret: consumerSecret)
@@ -28,7 +29,6 @@ class TwitterClient: BDBOAuth1SessionManager {
         
         loginSuccess = success
         loginFailure = failure
-        
         
         TwitterClient.sharedInstance?.deauthorize()
         TwitterClient.sharedInstance?.fetchRequestToken(withPath: requestTokenURL,
@@ -44,6 +44,15 @@ class TwitterClient: BDBOAuth1SessionManager {
         }
     }
     
+    
+    func logout() {
+        
+        User.currentUser = nil
+        deauthorize()
+        
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: User.userDidLogoutNotification), object: nil)
+    
+    }
     func handleOpenUrl(url: URL) {
         
         let requestToken = BDBOAuth1Credential(queryString: url.query)
@@ -54,12 +63,17 @@ class TwitterClient: BDBOAuth1SessionManager {
                                  success: { (accessToken: BDBOAuth1Credential?) -> Void in
                                     print("I got the access token!")
                                     
-                                    self.loginSuccess?()
-                                    
+                                    self.currentAccount(sucess: { (user: User) -> () in
+                                        User.currentUser = user
+                                        self.loginSuccess?()
+                                    }, failure: {(error: Error) -> () in
+                                        self.loginFailure?(error)
+                                        })
                                     
                                 }) { (error: Error?) -> Void in
                                         print("error: \(error?.localizedDescription)")
                                     self.loginFailure?(error!)
+
         }
         
     }
@@ -90,19 +104,21 @@ class TwitterClient: BDBOAuth1SessionManager {
     
     }
 
-    func currentAccount() {
+    func currentAccount(sucess: @escaping (User) -> (), failure: @escaping (Error) -> ()) {
         
         get("1.1/account/verify_credentials.json",
-                    parameters: nil,
-                    progress: nil,
-                    success: { (task: URLSessionDataTask?, response: Any?) -> Void in
-                        let userDictionary = response as! NSDictionary
-                        let user = User(dictionary: userDictionary)
-                        
-                        
+            parameters: nil,
+            progress: nil,
+            success: { (task: URLSessionDataTask?, response: Any?) -> Void in
+                let userDictionary = response as! NSDictionary
+                let user = User(dictionary: userDictionary)
+                
+                sucess(user)
+                
+                
         }, failure: { (task: URLSessionDataTask?, error: Error) -> Void in
             
-            
+            failure(error)
         })
     }
 }
--- TwitterClone/User.swift
+++ TwitterClone/User.swift
@@ -15,8 +15,11 @@ class User: NSObject {
     var profileUrl: URL?
     var tagline: String?
     
+    var dictionary: NSDictionary?
+    
     init(dictionary: NSDictionary){
         
+        self.dictionary = dictionary
         name = dictionary["name"] as? String
         screename = dictionary["screen_name"] as? String
         
@@ -26,7 +29,49 @@ class User: NSObject {
         }
         
         tagline = dictionary["description"] as? String
-        
     }
+    
+    static let userDidLogoutNotification = "UserDidLogout"
 
+    
+    static var _currentUser: User?
+    
+    
+    class var currentUser: User? {
+        get {
+            if _currentUser == nil{
+                let defaults = UserDefaults.standard
+                
+                let userData = defaults.object(forKey: "currentUserData") as? Data
+                
+                if let userData = userData {
+                    defaults.removeObject(forKey:"currentUserData")
+                    
+                    let dictionary = try!
+                        JSONSerialization.jsonObject(with: userData, options: []) as! NSDictionary
+                    _currentUser = User(dictionary: dictionary)
+                }
+            }
+            
+            return _currentUser
+        }
+        set(user) {
+            
+            _currentUser = user
+            
+            let defaults = UserDefaults.standard
+            
+            if let user = user {
+                
+                let data = try!
+                    JSONSerialization.data(withJSONObject: user.dictionary!, options: [])
+                defaults.set(data, forKey: "currentUserData")
+
+            } else {
+                defaults.set(nil, forKey: "currentUserData")
+            }
+            
+            defaults.synchronize()
+        }
+    }
 }
